564ac22a99754df4f68c504e0d24e69ab6cee112
Přemysl Černý <premysl.cerny@teskalabs.com>
2021-03-23 09:58:46 +0100

    Add one more iter test for document.

--- utest/__init__.py
+++ utest/__init__.py
@@ -5,17 +5,38 @@ import cysimdjson
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
+
 class JSONArrayTestCases(unittest.TestCase):
 
 	def test_iter_01(self):
-		
+
 		parser = cysimdjson.JSONParser()
 		
 		with open(os.path.join(THIS_DIR, 'array.json'), 'rb') as fo:
 			json_parsed = parser.parse(fo.read())
 
-
 		ar = json_parsed['array']
 		self.assertEqual(len(ar), 10)
 		for i, n in enumerate(ar, 1):
 			self.assertEqual(i, n)
+
+	def test_iter_02(self):
+
+		parser = cysimdjson.JSONParser()
+
+		with open(os.path.join(THIS_DIR, 'document.json'), 'rb') as fo:
+			json_parsed = parser.parse(fo.read())
+
+		_document = json_parsed['document']
+		_dict = {}
+
+		for key, value in _document.items():
+			_dict[key] = value
+
+		self.assertEqual({
+			"key1": 1,
+			"key2": "2",
+			"key3": "3",
+			"key4": 40,
+			"key5": "50",
+		}, _dict)
