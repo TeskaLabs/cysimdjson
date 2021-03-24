9a241946d447af0c36ccc6b45b01d08f8739a318
Přemysl Černý <premysl.cerny@teskalabs.com>
2021-03-24 15:08:52 +0100

    Fix typo.

--- cysimdjson/jsoninter.h
+++ cysimdjson/jsoninter.h
@@ -157,7 +157,7 @@ inline dom::object to_object(dom::element & value, int * ok) {
 
 inline PyObject * string_view_to_string(std::string_view sv) {
 	// This creates a string copy, which ensures termination by \0
-	std::string str = std::string(string_view_object);
+	std::string str = std::string(sv);
 
 	return PyUnicode_FromStringAndSize(
 		str.c_str(),
