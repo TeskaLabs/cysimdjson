e8d2517213f1f096508d156eeefdf9e3e8b475ff
Ales Teska <ales.teska@teskalabs.com>
2021-04-08 15:51:15 +0200

    Extends an official python version support.

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ setup(
 	platforms='any',
 	classifiers=[
 		'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python :: 3.8',
+		'Programming Language :: Python :: 3.9',
 	],
 	packages=[
 		"cysimdjson",
