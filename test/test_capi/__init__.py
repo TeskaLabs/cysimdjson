73ea94bea94c98761e53163ddb35dbdfa7ce97ed
Ales Teska <ales.teska@teskalabs.com>
2021-07-05 18:55:58 +0200

    More Linux-friendly foreing function handling.

--- test/test_capi/__init__.py
+++ test/test_capi/__init__.py
@@ -7,21 +7,24 @@ import cysimdjson
 class CySIMDJSONCAPITestCases(unittest.TestCase):
 
 
+	def setUp(self):
+		self.cysimdjsonapi = ctypes.cdll.LoadLibrary(cysimdjson.__file__)
+
 	def test_capi_01(self):
 
-		ctypes.pythonapi.cysimdjson_parser_new.restype = ctypes.c_void_p
-		ctypes.pythonapi.cysimdjson_parser_del.argtypes = [ctypes.c_void_p]
+		self.cysimdjsonapi.cysimdjson_parser_new.restype = ctypes.c_void_p
+		self.cysimdjsonapi.cysimdjson_parser_del.argtypes = [ctypes.c_void_p]
 
-		parser = ctypes.pythonapi.cysimdjson_parser_new()
+		parser = self.cysimdjsonapi.cysimdjson_parser_new()
 
-		ctypes.pythonapi.cysimdjson_parser_del(parser)
+		self.cysimdjsonapi.cysimdjson_parser_del(parser)
 
 
 	def test_capi_02(self):
-		ctypes.pythonapi.cysimdjson_parser_new.restype = ctypes.c_int
+		self.cysimdjsonapi.cysimdjson_parser_new.restype = ctypes.c_int
 		element_sizeof = ctypes.pythonapi.cysimdjson_element_sizeof()
 		print("element_sizeof:", element_sizeof)
 
 
 	def test_capi_03(self):
-		ctypes.pythonapi.cysimdjson_parser_test()
+		self.cysimdjsonapi.cysimdjson_parser_test()
