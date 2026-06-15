24f67d2f89c7265148c4892ca33fe7f8347d1430
Miroslav Burýšek <miroslav.burysek@teskalabs.com>
2026-06-15 13:51:13 +0200

    Add build for Python 3.13 and 3.14

--- build-linux-wheel.sh
+++ build-linux-wheel.sh
@@ -28,6 +28,14 @@ rm -rf build cysimdjson.egg-info
 /opt/python/cp312-cp312/bin/pip3 install Cython wheel
 /opt/python/cp312-cp312/bin/python3 setup.py bdist_wheel
 
+rm -rf build cysimdjson.egg-info
+/opt/python/cp313-cp313/bin/pip3 install setuptools Cython wheel
+/opt/python/cp313-cp313/bin/python3 setup.py bdist_wheel
+
+rm -rf build cysimdjson.egg-info
+/opt/python/cp314-cp314/bin/pip3 install setuptools Cython wheel
+/opt/python/cp314-cp314/bin/python3 setup.py bdist_wheel
+
 cd /cysimdjson/dist
 find . -name "cysimdjson-*-linux_x86_64.whl" | xargs -n 1 auditwheel repair -w /cysimdjson/dist
 rm cysimdjson-*-linux_x86_64.whl
